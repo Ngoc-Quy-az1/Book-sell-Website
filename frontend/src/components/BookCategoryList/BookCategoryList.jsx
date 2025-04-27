0b89b5089af43896bdd9d57b4e99c15e06ebc4d2
NHP-AYKM <167326970+NHP-AYKM@users.noreply.github.com>
2025-04-27 22:16:43 +0700

    booklist updated

--- frontend/src/components/BookCategoryList/BookCategoryList.jsx
+++ frontend/src/components/BookCategoryList/BookCategoryList.jsx
@@ -1,18 +1,58 @@
-import Navbar from "../Navbar/Navbar";
+import React from "react";
 import { useState,useEffect } from "react";
 import axios from 'axios';
+import Cookies from "js.cookie";
+import { Slider } from "@mui/material";
 
+const config = {'Authorization': `Bearer ${Cookies.get('authToken')}`}
+console.log(Cookies.get('authToken'))
 const BookCategoryList = () => {
-  const [selectedCountries, setSelectedCountries] = useState([]);
-  const category = ["Giáo dục", "Kinh tế", "Văn học", "Tiểu thuyết", "Thiếu nhi"];
-  const [bookList, setBooklist] = useState([]);
+  //Lấy tất cả thể loại
+  const [categories, setCategories] = useState([]);
+  useEffect( () => {
+    getBookCategory();
+  }, [])
+
+//Thể loại đang được chọn 
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const handleChange = (event) => {
+    const index = selectedCategory.indexOf(event.target.id);
+    (event.target.checked) ? setSelectedCategory([...selectedCategory, event.target.id])
+    : setSelectedCategory([...selectedCategory.slice(0,index),...selectedCategory.slice(index+1)])
+  }
+//Slider xét giá
+  const [value, setValue] = React.useState([0, 1000000]);
+
+  const handleSlider = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handleMinSlider = (event) => {
+    setValue([event.target.value === '' ? 0 : Number(event.target.value),value[1]]);
+  };
+  const handleMaxSlider = (event) => {
+    setValue([value[0],event.target.value === '' ? 0 : Number(event.target.value)]);
+  };
+  
+  const getBookCategory = async ()=>{
+    await fetch('http://localhost:8090/api/books/AllTypeCategories', {headers: config})
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      setCategories(response); 
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }
+  const [bookList, setBookList] = useState([]);
   useEffect( () => {
     getBookPage();
   }, []);
   const getBookPage = async ()=>{
     await axios.get('http://localhost:8090/api/books/GetAllPaginated')
     .then((response) => {
-        setBooklist(response.data.content);
+        setBookList(response.data.content);
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
@@ -29,7 +69,7 @@ const BookCategoryList = () => {
       }
     )
     .then((response) => {
-        setBooklist(response.data.content);
+        setBookList(response.data.content);
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
@@ -39,13 +79,45 @@ const BookCategoryList = () => {
     <div className="flex p-5 pl-40 pr-40">
       {/* Sidebar */}
       <div className="w-1/4 pr-5 border-r">
-        <h2 className="font-bold text-lg mb-2 text-green-600">Thể loại</h2>
-        {category.map((country) => (
-          <div key={country} className="flex items-center mb-2">
-            <input type="checkbox" className="mr-2" />
-            <span>{country}</span>
+        <h2 className="font-bold text-lg mb-2 text-green-600">Phi hư cấu</h2>
+        {categories.slice(0,10).map((category) => (
+          <div key={category} className="flex items-center mb-2">
+            <input type="checkbox" className="mr-2" id={category} onChange={handleChange}/>
+            <span>{category}</span>
           </div>
         ))}
+        <h2 className="font-bold text-lg mb-2 text-green-600">Hư cấu </h2>
+        {categories.slice(10,21).map((category) => (
+          <div key={category} className="flex items-center mb-2">
+            <input type="checkbox" className="mr-2" id={category} onChange={handleChange}/>
+            <span>{category}</span>
+          </div>
+        ))}
+        <h2 className="font-bold text-lg mb-2 text-green-600">Thiếu nhi</h2>
+        {categories.slice(21,25).map((category) => (
+          <div key={category} className="flex items-center mb-2">
+            <input type="checkbox" className="mr-2" id={category} onChange={handleChange}/>
+            <span>{category}</span>
+          </div>
+        ))}
+        
+        {/*Slider giá */}
+        <h2 className="font-bold text-lg mb-2 text-green-600">Giá</h2>
+          <Slider
+            getAriaLabel={(index) => (index === 0 ? 'Minimum price' : 'Maximum price')}
+            value={value}
+            onChange={handleSlider}
+            min={0}
+            step={10000}
+            max={1000000}
+            valueLabelDisplay="auto"
+          />
+        <div className="flex items-center mb-2 justify-center"> Giá tối thiểu: 
+          <input value={value[0]} className="search-input" onChange={handleMinSlider} style={{marginLeft:"5px", }}></input>
+        </div>
+        <div className="flex items-center mb-2 justify-center"> Giá tối đa:  
+          <input value={value[1]} className="search-input" onChange={handleMaxSlider} style={{marginLeft:"10px", }}></input>
+        </div>
       </div>
 
       {/* Main Content */}
@@ -67,19 +139,20 @@ const BookCategoryList = () => {
               <h1 className="font-bold text-xl mb-1">{book.title}</h1>
 
               <div className="text-xl font-bold text-green-600">
-              {(parseFloat(book.price_discounted)).toLocaleString(undefined,
-                {'minimumFractionDigits':3}
-              )}₫ 
-              <span className="text-base line-through text-gray-500 ml-2">
-                {(parseFloat(book.price_original)).toLocaleString(undefined,
-              {'minimumFractionDigits':3}  
-              )}₫</span>
-              <span className="bg-red-500 text-white text-sm font-medium px-2 py-1 rounded ml-2">
-                {Math.round( (parseFloat(book.price_original) - parseFloat(book.price_discounted))*100
-                  /parseFloat(book.price_original) )} %
-              </span>
-
-            </div>
+                {(parseFloat(book.price_discounted)).toLocaleString(undefined,
+                  {'minimumFractionDigits':3}
+                )}₫ 
+              </div>
+              <div>
+                <span className="text-base line-through text-gray-500 ml-2">
+                  {(parseFloat(book.price_original)).toLocaleString(undefined,
+                {'minimumFractionDigits':3}  
+                )}₫</span>
+                <span className="bg-red-500 text-white text-sm font-medium px-2 py-1 rounded ml-2">
+                  {Math.round( (parseFloat(book.price_original) - parseFloat(book.price_discounted))*100
+                    /parseFloat(book.price_original) )} %
+                </span>
+              </div>
               
             </div>
           ))}
--- frontend/src/scenes/admin/global/Topbar.jsx
+++ frontend/src/scenes/admin/global/Topbar.jsx
@@ -1,13 +1,11 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../../theme";
-import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 import Cookies from "js.cookie"
 
 const Topbar = ({handleNotification}) => {
@@ -43,10 +41,6 @@ const Topbar = ({handleNotification}) => {
         backgroundColor={colors.primary[900]}
         borderRadius="3px"
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
       </Box>
 
       {/* ICONS */}
